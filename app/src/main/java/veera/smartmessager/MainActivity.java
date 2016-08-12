313a6fe9e38dd93cc4ccf70710b28d5b0319de5b
VeeraAnudeep <veera.anudeep@toppr.com>
2016-08-13 00:48:23 +0530

    drive sync added

--- app/src/main/java/veera/smartmessager/MainActivity.java
+++ app/src/main/java/veera/smartmessager/MainActivity.java
@@ -3,10 +3,13 @@ package veera.smartmessager;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.Telephony;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
@@ -16,8 +19,33 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi;
+import com.google.android.gms.drive.DriveContents;
+import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.DriveId;
+import com.google.android.gms.drive.DriveResource;
+import com.google.android.gms.drive.Metadata;
+import com.google.android.gms.drive.MetadataChangeSet;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,7 +53,12 @@ import veera.smartmessager.marshmallowPermissions.ActivityManagePermission;
 import veera.smartmessager.marshmallowPermissions.PermissionResult;
 import veera.smartmessager.marshmallowPermissions.PermissionUtils;
 
-public class MainActivity extends ActivityManagePermission implements MainView, SearchView.OnQueryTextListener, LoaderManager.LoaderCallbacks<Cursor> {
+public class MainActivity extends ActivityManagePermission implements MainView,
+        SearchView.OnQueryTextListener, LoaderManager.LoaderCallbacks<Cursor>,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+    public static final String TAG = "loo";
 
     public static final int CURSOR_ID = 1;
     @BindView(R.id.fab)
@@ -34,7 +67,14 @@ public class MainActivity extends ActivityManagePermission implements MainView, 
     @BindView(R.id.recyclerView)
     RecyclerView recyclerView;
 
-    private MainPresenter mainPresenter;
+    private static final int REQUEST_CODE = 101;
+    public ArrayList<String> smsBuffer = new ArrayList<>();
+    String smsFile = "SMS" + ".csv";
+    File backUpSms;
+    private GoogleApiClient googleApiClient;
+    public static String drive_id;
+    public static DriveId driveID;
+
     private MainAdapter mainAdapter;
     final Uri CONTENT_URI = Telephony.Sms.CONTENT_URI;
     final String SORT_ORDER = Telephony.Sms.DEFAULT_SORT_ORDER;
@@ -57,9 +97,9 @@ public class MainActivity extends ActivityManagePermission implements MainView, 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mainPresenter = new MainPresenterImpl(this);
         mainAdapter = new MainAdapter(this);
         recyclerView.setAdapter(mainAdapter);
+        fab.setOnClickListener(this);
     }
 
     private void checkForPermissions() {
@@ -102,7 +142,26 @@ public class MainActivity extends ActivityManagePermission implements MainView, 
             return true;
         }
 
+        if (id == R.id.sync) {
+            checkForStoragePermissions();
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private void checkForStoragePermissions() {
+        askCompactPermissions(new String[]{PermissionUtils.Manifest_READ_EXTERNAL_STORAGE, PermissionUtils.Manifest_WRITE_EXTERNAL_STORAGE}, new PermissionResult() {
+            @Override
+            public void permissionGranted() {
+                buildGoogleApiClient();
+            }
+
+            @Override
+            public void permissionDenied() {
+                finish();
+            }
+        });
     }
 
     @Override
@@ -162,5 +221,220 @@ public class MainActivity extends ActivityManagePermission implements MainView, 
             getSupportLoaderManager().restartLoader(CURSOR_ID, null, this);
         }
         return false;
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (googleApiClient != null) {
+            googleApiClient.disconnect();
+        }
+    }
+
+
+    /*Handles onConnectionFailed callbacks*/
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            googleApiClient.connect();
+        }
+    }
+
+    /* *//*handles connection callbacks*/
+    @Override
+    public void onConnected(Bundle bundle) {
+        Drive.DriveApi.newDriveContents(googleApiClient).setResultCallback(driveContentsCallback);
+    }
+
+    /*handles suspended connection callbacks*/
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.i("v", "Error creating new file contents");
+    }
+
+    /*callback on getting the drive contents, contained in result*/
+    final private ResultCallback<DriveApi.DriveContentsResult> driveContentsCallback = new
+            ResultCallback<DriveApi.DriveContentsResult>() {
+                @Override
+                public void onResult(@NonNull DriveApi.DriveContentsResult result) {
+                    if (!result.getStatus().isSuccess()) {
+                        Log.i(TAG, "Error creating new file contents");
+                        return;
+                    }
+                    final DriveContents driveContents = result.getDriveContents();
+                    new Thread() {
+                        @Override
+                        public void run() {
+                            OutputStream outputStream = driveContents.getOutputStream();
+                            backupSMS();
+                            addTextfileToOutputStream(outputStream);
+                            MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
+                                    .setTitle("testFile")
+                                    .setMimeType("text/plain")
+                                    .setDescription("This is a text file uploaded from device")
+                                    .setStarred(true).build();
+                            Drive.DriveApi.getRootFolder(googleApiClient)
+                                    .createFile(googleApiClient, changeSet, driveContents)
+                                    .setResultCallback(fileCallback);
+                        }
+
+                    }.start();
+                }
+            };
+
+    /*get input stream from text file, read it and put into the output stream*/
+    private void addTextfileToOutputStream(OutputStream outputStream) {
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        try {
+            BufferedInputStream inputStream = new BufferedInputStream(
+                    new FileInputStream(backUpSms));
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            Log.i(TAG, "problem converting input stream to output stream: " + e);
+            e.printStackTrace();
+        }
+    }
+
+    /*callback after creating the file, can get file info out of the result*/
+    final private ResultCallback<DriveFolder.DriveFileResult> fileCallback = new
+            ResultCallback<DriveFolder.DriveFileResult>() {
+                @Override
+                public void onResult(@NonNull DriveFolder.DriveFileResult result) {
+                    if (!result.getStatus().isSuccess()) {
+                        Log.i(TAG, "Error creating the file");
+                        Toast.makeText(MainActivity.this,
+                                "Error adding file to Drive", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    Log.i(TAG, "File added to Drive");
+                    Log.i(TAG, "Created a file with content: "
+                            + result.getDriveFile().getDriveId());
+                    Toast.makeText(MainActivity.this,
+                            "File successfully added to Drive", Toast.LENGTH_SHORT).show();
+                    final PendingResult<DriveResource.MetadataResult> metadata
+                            = result.getDriveFile().getMetadata(googleApiClient);
+                    metadata.setResultCallback(new ResultCallback<DriveResource.MetadataResult>() {
+                        @Override
+                        public void onResult(@NonNull DriveResource.MetadataResult metadataResult) {
+                            Metadata data = metadataResult.getMetadata();
+                            Log.i(TAG, "Title: " + data.getTitle());
+                            drive_id = data.getDriveId().encodeToString();
+                            Log.i(TAG, "DrivId: " + drive_id);
+                            driveID = data.getDriveId();
+                            Log.i(TAG, "Description: " + data.getDescription());
+                            Log.i(TAG, "MimeType: " + data.getMimeType());
+                            Log.i(TAG, "File size: " + String.valueOf(data.getFileSize()));
+                        }
+                    });
+                }
+            };
+
+    /*callback when there there's an error connecting the client to the service.*/
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult result) {
+        if (!result.hasResolution()) {
+            GoogleApiAvailability.getInstance().getErrorDialog(this, result.getErrorCode(), 0).show();
+            return;
+        }
+        try {
+            result.startResolutionForResult(this, REQUEST_CODE);
+        } catch (IntentSender.SendIntentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*build the google api client*/
+    private void buildGoogleApiClient() {
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(Drive.API)
+                    .addScope(Drive.SCOPE_FILE)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+            googleApiClient.connect();
+        }
+    }
+
+    private void backupSMS() {
+        Uri mSmsinboxQueryUri = Uri.parse("content://sms");
+        Cursor cursor1 = getContentResolver().query(
+                mSmsinboxQueryUri,
+                new String[]{"_id", "thread_id", "address", "person", "date",
+                        "body", "type"}, null, null, null);
+        //startManagingCursor(cursor1);
+        String[] columns = new String[]{"_id", "thread_id", "address", "person", "date", "body",
+                "type"};
+        if (cursor1 != null) {
+            if (cursor1.getCount() > 0) {
+                String count = Integer.toString(cursor1.getCount());
+                Log.d("Count", count);
+                while (cursor1.moveToNext()) {
+
+                    String messageId = cursor1.getString(cursor1
+                            .getColumnIndex(columns[0]));
+
+                    String threadId = cursor1.getString(cursor1
+                            .getColumnIndex(columns[1]));
+
+                    String address = cursor1.getString(cursor1
+                            .getColumnIndex(columns[2]));
+                    String name = cursor1.getString(cursor1
+                            .getColumnIndex(columns[3]));
+                    String date = cursor1.getString(cursor1
+                            .getColumnIndex(columns[4]));
+                    String msg = cursor1.getString(cursor1
+                            .getColumnIndex(columns[5]));
+                    String type = cursor1.getString(cursor1
+                            .getColumnIndex(columns[6]));
+
+
+                    smsBuffer.add(messageId + "," + threadId + "," + address + "," + name + "," + date + " ," + msg + " ,"
+                            + type);
+
+                }
+                cursor1.close();
+                generateCSVFileForSMS(smsBuffer);
+            }
+        }
+    }
+
+
+    private void generateCSVFileForSMS(ArrayList<String> list) {
+
+        try {
+            backUpSms = new File(Environment.getExternalStorageDirectory()
+                    + File.separator + smsFile);
+            String storage_path = Environment.getExternalStorageDirectory().toString() + File.separator + smsFile;
+            FileWriter write = new FileWriter(storage_path);
+
+            write.append("messageId, threadId, Address, Name, Date, msg, type");
+            write.append('\n');
+
+            for (String s : list) {
+                write.append(s);
+                write.append('\n');
+            }
+            write.flush();
+            write.close();
+            smsBuffer.clear();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.fab:
+                startActivity(new Intent(this, NewMessageActivity.class));
+                break;
+        }
     }
 }
